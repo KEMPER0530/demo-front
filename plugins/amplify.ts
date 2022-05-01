3b0dcf19c4c548dc1766ce03cff2db46069d5430
Y.Akazawa <yoshi_akazawa_sto@RED-iMac-3.local>
2022-05-01 10:08:58 +0900

    [ts-migrate][.] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

--- plugins/amplify.ts
+++ plugins/amplify.ts
@@ -3,7 +3,6 @@ import Amplify, * as AmplifyModules from 'aws-amplify'
 // @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'aws-... Remove this comment to see the full error message
 import { AmplifyPlugin, components } from 'aws-amplify-vue'
 import Auth from '@aws-amplify/auth';
-// @ts-expect-error ts-migrate(2307) FIXME: Cannot find module '@/src/appsync-exports' or its ... Remove this comment to see the full error message
 import awsconfig from '@/src/appsync-exports'
 
 export default () => {
--- src/appsync-exports.ts
+++ src/appsync-exports.ts
@@ -1,15 +1,9 @@
-// @ts-expect-error ts-migrate(2580) FIXME: Cannot find name 'require'. Do you need to install... Remove this comment to see the full error message
 require('dotenv').config();
 const awsmobile = {
-    // @ts-expect-error ts-migrate(2580) FIXME: Cannot find name 'process'. Do you need to install... Remove this comment to see the full error message
     aws_project_region: process.env.AWS_APPSYNC_REIGION,
-    // @ts-expect-error ts-migrate(2580) FIXME: Cannot find name 'process'. Do you need to install... Remove this comment to see the full error message
     aws_appsync_graphqlEndpoint: process.env.AWS_APPSYNC_GRAPHQL_ENDPOINT,
-    // @ts-expect-error ts-migrate(2580) FIXME: Cannot find name 'process'. Do you need to install... Remove this comment to see the full error message
     aws_appsync_region: process.env.AWS_APPSYNC_REIGION,
-    // @ts-expect-error ts-migrate(2580) FIXME: Cannot find name 'process'. Do you need to install... Remove this comment to see the full error message
     aws_appsync_authenticationType: process.env.AWS_APPSYNC_AUTHENTICATION_TYPE,
-    // @ts-expect-error ts-migrate(2580) FIXME: Cannot find name 'process'. Do you need to install... Remove this comment to see the full error message
     aws_appsync_apiKey: process.env.AWS_APPSYNC_APIKEY
 }
 
